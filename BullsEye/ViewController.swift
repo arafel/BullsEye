d3f0c56e347d5f9baef72c6b49dea02c2250823a
Paul Walker <paul@blacksun.org.uk>
2017-02-26 23:24:25 +0000

    Slider now actually does something vaguely useful ('hit me' prints value)

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +24,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)"
         let alert = UIAlertController(title: "Hello, world",
-                                      message: "This is my first app!",
+                                      message: message,
                                       preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let action = UIAlertAction(title: "OK",
+                                   style: .default, handler: nil)
         
         alert.addAction(action)
 
         present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
     }
 }
 
